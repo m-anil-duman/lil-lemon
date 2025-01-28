8a43293d5fe7ede89d6b5015811e50b314323282
m-anil-duman <anil.duman00@gmail.com>
2025-01-28 18:42:41 +0100

    bug fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,13 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "plugin:@typescript-eslint/recommended"),
+  {
+    ignores: [".next/**", "node_modules/**"],
+    rules: {
+      "react/display-name": "off",
+    },
+  },
+];
 export default eslintConfig;
--- src/app/api/menu/route.js
+++ src/app/api/menu/route.js
@@ -1,13 +1,12 @@
 // app/api/menu/route.js
-export async function GET(req) {
-    const response = await fetch('https://little-lemon-restaurant-database.onrender.com/menu');
-    if (!response.ok) {
-      return new Response('Failed to fetch menu data', { status: 500 });
-    }
-  
-    const data = await response.json();
-    return new Response(JSON.stringify(data), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+export async function GET() {
+  const response = await fetch('https://little-lemon-restaurant-database.onrender.com/menu');
+  if (!response.ok) {
+    return new Response('Failed to fetch menu data', { status: 500 });
   }
-  
+
+  const data = await response.json();
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
