12cf9f88eeb559f3a518a12bd52346282a3e5970
m-anil-duman <anil.duman00@gmail.com>
2025-02-07 19:03:47 +0100

    bug fix

--- src/app/api/menu/route.js
+++ src/app/api/menu/route.js
@@ -1,5 +1,5 @@
 // app/api/menu/route.js
-export async function GET() {
+export default async function GET() {
   const response = await fetch('https://little-lemon-restaurant-database.onrender.com/menu');
   if (!response.ok) {
     return new Response('Failed to fetch menu data', { status: 500 });
